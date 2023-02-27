18610e9fab208a76b6fa5d193e5eb8847116c02a
yago-rayon <yago.rayon@proton.me>
2023-02-27 11:47:25 +0100

    Tabla creada

--- src/app/componentes/tabla-autores/tabla-autores.component.ts
+++ src/app/componentes/tabla-autores/tabla-autores.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, Directive, EventEmitter, Input, Output, QueryList, ViewChildren, OnInit } from '@angular/core';
+import { DecimalPipe, NgFor } from '@angular/common';
+import { Router } from '@angular/router';
 //Servicios
 import { AutoresService } from 'src/app/servicios/autores.service';
+//Interfaces
+import { Autor } from '../../interfaces/Autor';
+
 @Component({
   selector: 'app-tabla-autores',
   templateUrl: './tabla-autores.component.html',
   styleUrls: ['./tabla-autores.component.scss']
 })
 export class TablaAutoresComponent {
+  autores: Autor[]=[];
 
+  constructor(private servicioAutores:AutoresService,private router: Router){}
+
+  ngOnInit(){
+    this.consultarAutores();
+  }
+  consultarAutores(){
+    this.servicioAutores.consultarTodosAutores()
+                       .subscribe( datos => this.autores = datos);
+  }
+  borrar(autor:Autor){
+    this.servicioAutores.borrarAutor(autor)
+                       .subscribe (respuesta=>{
+                        if (respuesta.status=="success"){
+                          alert("autor borrado");
+                          this.consultarAutores();
+                        } else {
+                          alert("No se ha podido borrar el autor");
+                        }
+                       });
+  }
+  editar(autor:Autor){
+    this.router.navigate(['/editarAutor/'+autor._id]);
+  }
 }
--- src/app/servicios/autores.service.ts
+++ src/app/servicios/autores.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
-
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+//interfaces
+import { Autor } from '../interfaces/Autor';
 @Injectable({
   providedIn: 'root'
 })
 export class AutoresService {
+  urlAPI="http://localhost:8001";
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  consultarTodosAutores():Observable<Autor[]>{
+    return this.http.get<Autor[]>(this.urlAPI + "/autores");
+  }
+  consultarAutor(id:string):Observable<Autor>{
+    return this.http.get<Autor>(this.urlAPI + "/autor/" + id);
+  }
+  altaAutor(autor:Autor):Observable<any>{
+    return this.http.post<any>(this.urlAPI + "/autores",autor,this.httpOptions);
+  }
+  editarAutor(autor:Autor):Observable<any>{
+    return this.http.put(this.urlAPI + "/autores",autor,this.httpOptions)
+  }
+  borrarAutor(autor:Autor):Observable<any>{
+    return this.http.delete(this.urlAPI + "/autores/" + autor._id)
+  }
 }
