c1c25641d7bc98a13d2f13b3ca786174954e8ff7
yago-rayon <yago.rayon@proton.me>
2023-06-14 16:01:40 +0200

    Cambios para la api en produccion

--- src/app/constantes/constantes.ts
+++ src/app/constantes/constantes.ts
@@ -1,7 +1,7 @@
 export const Constantes = {
-    directorioImagenes:'/api/public/img/',
-    directorioIconos:'/api/public/iconos/',
-    urlApi:'/api',
+    directorioImagenes:'https://galaxia-libros-node.onrender.com/api/public/img/',
+    directorioIconos:'https://galaxia-libros-node.onrender.com/api/public/iconos/',
+    urlApi:'https://galaxia-libros-node.onrender.com/api',
     listaGeneros:['Acción','Aventura','Ciencia Ficción','Comedia','Contemporaneo','Drama','Fantasía','Histórico','Horror','Misterio','Psicológico','Romance','Sátirico','Tragedia'],
     listaEtiquetas:['Inteligencia Artificial','Protagonista Atractivo','Mazmorras','Protagonista Femenino','Protagonista Masculino','LitRPG','Artes Marciales','Varios Protagonistas',
     'Progresión','Viaje en el tiempo','Realidad Virtual','Super Héroes','Deportes','Reencarnación','Protagonista no humano']
